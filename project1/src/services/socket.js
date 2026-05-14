3a0eaa08d193796a45359189e83bf241a4446d2a
Nandanmotato123 <naikn041@gmail.com>
2026-05-14 22:15:19 +0530

    Update socket.js

--- project1/src/services/socket.js
+++ project1/src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 
 // Get the socket URL from environment variables, or use default localhost URL
-let SOCKET_URL = 'https://bidmaster-backend.onrender.com'
+let SOCKET_URL = 'https://bidmaster-backend-ierg.onrender.com'
 if (import.meta.env && import.meta.env.VITE_SOCKET_URL) {
   SOCKET_URL = import.meta.env.VITE_SOCKET_URL
 }
